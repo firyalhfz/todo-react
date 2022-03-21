e1d25618caab34cea3e1360dfba4cb66d8cbb9ba
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-21 21:31:43 +0700

    add: todo create

---
+++ src/components/todo-create/TodoCreate.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './TodoCreate.css'
+
+const TodoCreate = (props) => {
+  const[getInputTodo, setInputTodo] = useState('')
+
+  const handleSubmit = (event) => {
+    //mencegah perilaku bawaan,
+    event.preventDefault()
+
+    const newTodo = {
+      id: Math.floor(Math.random() * 100) +1,
+      title: getInputTodo
+    }
+
+    props.onCreateTodo(newTodo)
+    setInputTodo('')
+
+    // console.log(newTodo)
+  }
+
+  const handleInputTodo = (event) => {
+    setInputTodo(event.target.value)
+    console.log(getInputTodo)
+  }
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input type="text" value={getInputTodo} onChange={handleInputTodo}></input>
+      <button type="submit">Add</button>
+    </form>
+  )
+}
+
+export default TodoCreate
